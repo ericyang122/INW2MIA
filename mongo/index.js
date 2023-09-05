3e78586b23989b36f96d7b576bd4609d98c4ea82
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-05 13:19:12 -0300

    conectar com o banco

---
+++ mongo/index.js
@@ -0,0 +1,44 @@
+//mongodb://localhost:27017
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/exemplo",
+{useNewUrlParser : true,
+ useUnifiedTopology:true,
+ serverSelectionTimeoutMS: 10000
+});
+
+const db = mongoose.connection
+
+db.on("error", console.error.bind(console,"connection error : "))
+
+db.once("open" , function(){
+    console.log("Estamos conectados ao banco")
+})
+const pessoaSchema = new mongoose.Schema({
+    nome : String,
+    idade : Number,
+    profissao : String 
+});
+const pessoa = mongoose.model('pessoa',pessoaSchema);
+const marcos = new pessoa({
+    nome : 'marcos',
+    idade : 25,
+    profissao : 'odontologo'
+});
+const jessica = new pessoa({
+    nome : 'jessica',
+    idade : 13,
+    profissao : 'estudante'
+});
+
+console.log(marcos.nome);
+console.log(marcos.idade);
+console.log(marcos.profissao);
+marcos.save();
+
+console.log(jessica.nome);
+console.log(jessica.idade);
+console.log(jessica.profissao);
+jessica.save();
+
---
+++ mongo/readme.md
@@ -0,0 +1,2 @@
+apos clonar execute 
+npm install
